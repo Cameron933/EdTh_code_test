b687f789b17d4813f4191bfa034bf3a11896f7a6
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-09-01 21:27:13 +1000

    feat: install axios

--- src/components/StudentTable.tsx
+++ src/components/StudentTable.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer } from "@chakra-ui/react";
+import axios from "axios";
 
 const StudentTable = () => {
-  return <div></div>;
+  return <div>Student Table place holder</div>;
 };
 
 export default StudentTable;
