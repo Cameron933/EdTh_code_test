dd84df7f65307bc7b0b2375a1338b2f836dca2fc
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-09-01 21:30:01 +1000

    feat: fetching student data

--- src/components/StudentTable.tsx
+++ src/components/StudentTable.tsx
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer } from "@chakra-ui/react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const StudentTable = () => {
+  const [studentInfo, setStudentInfo] = useState<StudentInfo[]>([]);
+
+  const fetchStudentsInfo = async () => {
+    try {
+      const responseStudents = await axios.get("http://localhost:5000/students");
+
+      if (responseStudents && responseStudents.data) {
+        setStudentInfo(responseStudents.data);
+      }
+    } catch (error) {
+      const { response, message } = error as AxiosError;
+      console.log(message);
+      console.log(response?.status);
+    }
+  };
+
+  useEffect(() => {
+    fetchStudentsInfo();
+  }, []);
+
+  console.log(studentInfo);
+
   return <div>Student Table place holder</div>;
 };
 
--- src/interfaces/student.d.ts
+++ src/interfaces/student.d.ts
@@ -1,3 +1,16 @@
-interface Student {
-  name: string;
+interface StudentInfo {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  date_of_birth: string;
+  address: Address;
+}
+
+interface Address {
+  street_line1: string;
+  street_line2: string;
+  country: string;
+  postcode: string;
+  suburb?: string;
 }
