14f8b982426702fbdd8d64748423d1fa613da10e
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-09-01 19:31:27 +1000

    project init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{backgroundColor: "#2546ce", height: "100vh"}}>
+      <div style={{backgroundColor: "#17e07d", color: "white"}}>
+          <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center', height: '20%', fontSize: 30 }}>Welcome to the Education Horizons </div>
+        </div>
+        <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center', height: '20%', fontSize: 20 }}>Frontend Code Challenge</div>
+    </div>
+  );
+}
+
+export default App;
